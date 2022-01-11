930c15cf7a0e8cc6f859c2699e8dc400c367c3f9
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-11 20:15:09 +0000

    add power draw to part

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
   public static void main(String[] args)
   {
     Computer comp = new Computer();
-    Part cpu = new Part("Xeon e5 2690 v3", 54.24f);
+    Part cpu = new Part("Xeon e5 2690 v3", 54.24f, 180.0f);
 
     comp.addPartToList(cpu);
 
--- Part.java
+++ Part.java
@@ -2,22 +2,25 @@ public class Part
 {
   private String name;
   private float cost;
+	private float powerDraw;
 
   public Part()
   {
     name = "";
     cost = 0.0f;
+		powerDraw = 0.0f;
   }
 
-  public Part(String _name, float _cost)
+  public Part(String _name, float _cost, float _power)
   {
     name = _name;
     cost = _cost;
+		powerDraw = _power;
   }
 
   public String toString()
   {
-    return name + ", Price: " + cost;
+    return name + ", Price: $" + cost + ", Power Draw: " + powerDraw + "w";
   }
 
   public String getName()
