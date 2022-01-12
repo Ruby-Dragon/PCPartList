47d4e9a5a90646fa22591d23fcff4bf0341a8dc0
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-12 18:05:12 +0000

    add getter and setter for power draw

--- Part.java
+++ Part.java
@@ -1,9 +1,11 @@
 public class Part
 {
+	//variables
   private String name;
   private float cost;
 	private float powerDraw;
 
+	//constructors
   public Part()
   {
     name = "";
@@ -18,11 +20,13 @@ public class Part
 		powerDraw = _power;
   }
 
+	//toString override
   public String toString()
   {
     return name + ", Price: $" + cost + ", Power Draw: " + powerDraw + "w";
   }
 
+	//getters and setters
   public String getName()
   {
     return name;
@@ -42,4 +46,14 @@ public class Part
   {
     cost = _cost;
   }
+
+	public void setPowerDraw(float _power)
+	{
+		powerDraw = _power;
+	}
+
+	public float getPowerDraw()
+	{
+		return powerDraw;
+	}
 }
