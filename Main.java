1fdad29baf1f177c956b70859707d00eaa93fc01
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-18 20:17:11 +0000

    add clear screen

--- Main.java
+++ Main.java
@@ -1,7 +1,31 @@
+import java.io.IOException;
+
 public class Main
 {
+	public static void clearScreen(){
+    //Clears Screen in java
+    try 
+		{
+				//windows clear screen
+        if (System.getProperty("os.name").contains("Windows"))
+				{
+					new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+				}
+        else
+				{
+					//Linux\Unix clear screen (probably the best idea)
+					Runtime.getRuntime().exec("clear");
+				} 
+    } 
+		catch (IOException | InterruptedException ex) 
+		{
+			ex.printStackTrace();
+		}
+	}
+
   public static void main(String[] args)
   {
+		//testing
     Computer comp = new Computer();
     Part cpu = new Part("Xeon e5 2690 v3", 54.24f, 180.0f);		
 		Part gpu = new Part("GTX 680", 200.0f, 195.0f, "https://www.ebay.com/itm/185251093760");
@@ -15,8 +39,9 @@ public class Main
 		Storage.openFile("mySickBuild.lst");
 		Storage.writeFile(comp);
 
-    
-
 		System.out.println(Storage.readFile());
+
+		clearScreen();
+		//interface loop
   }
 }
