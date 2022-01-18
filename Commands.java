e9247008d828791148d3da6661a03a9c90a192e9
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-18 21:45:37 +0000

    add readCommand function

--- Commands.java
+++ Commands.java
@@ -2,9 +2,37 @@ public class Commands
 {
 	public static void readCommand(String command)
 	{
+		int argc = 0;
 		for (int i = 0; i < command.length(); i++)
 		{
+			if (command.charAt(i) == ' ')
+			{
+				argc++;
+			}
+		}
+		String[] argv = command.split(" ", argc);
 
+		switch (argv[0])
+		{
+			case "open":
+				//something
+				break;
+
+			case "close":
+				//exit program
+				break;
+
+			case "add":
+				//add part
+				break;
+
+			case "rm":
+				//remove part
+				break;
+
+			case "purchase":
+				//purchase part
+				break;
 		}
 	}
 }
