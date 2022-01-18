0b5befaa8add146fff73d3cf79881163a198e7f8
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-18 20:00:44 +0000

    add more examples

--- Main.java
+++ Main.java
@@ -5,12 +5,14 @@ public class Main
     Computer comp = new Computer();
     Part cpu = new Part("Xeon e5 2690 v3", 54.24f, 180.0f);		
 		Part gpu = new Part("GTX 680", 200.0f, 195.0f, "https://www.ebay.com/itm/185251093760");
+		Part psu = new Part("Corsair CX750", 80.0f, 0.0f);
 
     comp.addPartToList(cpu);
 		comp.addPartToList(gpu);
+		comp.addPartToList(psu);
 		comp.setName("comp");
 
-		Storage.openFile("urmom.lst");
+		Storage.openFile("mySickBuild.lst");
 		Storage.writeFile(comp);
 
     
