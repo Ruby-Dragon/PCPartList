eb86c9cd864cac4da50221c20201d5479580df9e
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-25 20:14:53 +0000

    add JAR packaging to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+if [ ! -d "builds/lists" ]
+then
+	mkdir "builds/lists"
+	echo "made lists directory"
+fi
+
 javac -d builds @sources.txt
+
+echo "Manifest-Version: 1.0" > ./builds/manifest.mf
+echo "Main-Class: Main" >> ./builds/manifest.mf
+
+jar cfm ./builds/PCPartList.jar ./builds/manifest.mf *.class
+
+sleep 0.5
